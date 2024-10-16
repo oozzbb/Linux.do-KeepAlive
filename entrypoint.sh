f98e9360ff55ec4107a84333a380c62d935711c8
oozzbb <yangyijun37@hotmail.com>
2024-10-16 18:01:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,17 @@
+#!/bin/sh
+
+# 启用错误检测
+set -e
+
+# 定义错误处理函数
+error_handler () {
+    echo "[$(date +'% Y-% m-% d % H:% M:% S')] [ERROR] 脚本在第 ${LINENO} 行发生错误"
+    exit 1
+}
+
+# 捕获 ERR 信号，调用错误处理函数
+trap 'error_handler' ERR
+
 python Linux.do.py
+
+exec "$@"
