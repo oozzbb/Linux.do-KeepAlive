2fd5dccbf3e4dabdb45ad1e7289dd0fbe27e2a95
oozzbb <yangyijun37@hotmail.com>
2024-10-17 09:53:37 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,21 @@ error_handler () {
 # 捕获 ERR 信号，调用错误处理函数
 trap 'error_handler' ERR
 
+# 校验环境变量参数，对部分必要参数进行校验
+if [ -z $LINUXDO_USERNAME ]; then
+    echo "[$(date +'% Y-% m-% d % H:% M:% S')] [ERROR] 缺少 LINUXDO_USERNAME"
+    exit 1
+fi
+
+if [ -z $LINUXDO_PASSWORD ]; then
+    echo "[$(date +'% Y-% m-% d % H:% M:% S')] [ERROR] 缺少 LINUXDO_PASSWORD"
+    exit 1
+fi
+
+if [ -n $CRON_RULE ]; then
+    echo "$CRON_RULE /app/entrypoint.sh 2>&1" > /var/spool/cron/crontabs/root
+fi
+
 python Linux.do.py
 
 exec "$@"
